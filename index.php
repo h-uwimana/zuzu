0984179cde2b77557a3e5b897eac8b49598b0eea
h-uwimana <302211792@student.rocmondriaan.nl>
2022-09-01 20:46:54 +0200

    header, and half of the body made

--- index.php
+++ index.php
@@ -1,7 +1,90 @@
-<html>
+<html lang="html">
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
 </head>
+<body>
+<!--navbar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-dark ">
+    <div class="container-fluid">
+        <a class="navbar-brand text-light" href="#">ZUZU</a>
+        <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+                <a class="nav-link active text-light" href="#">Bestellen</a>
+            </div>
+        </div>
+    </div>
+</nav>
 
+<section class="bg-success container-fluid pt-5">
+    <div class="pt-5"></div>
+</section>
+<section class=" container-sm  mt-3">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1>
+                Goedemiddag, welkom bij ZuZu
+            </h1>
+        </div>
+        <div class="col-12 text-center">
+            <span class="fw-lighter">Wij zijn gespecialiseerd  in de Japanse keuken</span>
+            <p class="fw-lighter"> Het woord "sushi" is afkomstig van "su", wat azijn betekent en "shi" - rijst</p>
+        </div>
+        <div class="col-12 text-center">
+
+            <span>
+                <?php
+                $time = date("H:i");
+                $fmt = datefmt_create(
+                    'nld_nld',
+                    IntlDateFormatter::FULL,
+                    IntlDateFormatter::NONE,
+                    'Europe/Amsterdam',
+                    IntlDateFormatter::GREGORIAN,
+                );
+                $formatted_date = datefmt_format($fmt, time());
+
+                echo "Vandaag " . $formatted_date;
+
+                ?>
+            </span>
+            <p class="">
+                Bezorgtijd vanaf nu: <?php echo $time; ?>
+            </p>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col d-flex justify-content-center">
+            <div class="card " style="width: 20rem;
+            ">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <span class="card-text">Bestel bij ons je sushi</span>
+            </div>
+        </div>
+    </div>
+    <div class="col d-flex justify-content-center">
+        <div class="card "  style="width: 20rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <span class="card-text">Keuze uit verschillende soorten sushi's</span>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+</body>
+<script>
+
+</script>
 </html>
