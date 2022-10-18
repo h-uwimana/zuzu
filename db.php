b2295999e841e124a85c7f4d28233f466c7db7e3
h-uwimana <302211792@student.rocmondriaan.nl>
2022-10-18 14:37:06 +0200

    chatbox is klaar

--- db.php
+++ db.php
@@ -1,8 +1,7 @@
 <?php
 	
 	$db = new PDO("mysql:host=localhost; dbname=zuzu", "root", "");
-	$adresId = $db->prepare("SELECT id FROM `adres` WHERE straatnaam=:straatnaam AND postcode=:postcode   ");
-	
+	$chat = new PDO("mysql:host=localhost; dbname=chatbox", "root", "");
 	
 	
 	
